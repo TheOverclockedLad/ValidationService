2b4f6195e6f7f254cdfd541d49262a5b0068b438
TheOverclockedLad <42649194+TheOverclockedLad@users.noreply.github.com>
2019-03-12 19:55:19 +0530

    On branch Development Initial commit Changes to be committed: new file:   .gitignore new file:   ValidationService.Tests/Properties/AssemblyInfo.cs new file:   ValidationService.Tests/TestAbstractClassValidator.cs new file:   ValidationService.Tests/ValidationService.Tests.csproj new file:   ValidationService.sln new file:   ValidationService/App_Start/WebApiConfig.cs new file:   ValidationService/Global.asax new file:   ValidationService/Global.asax.cs new file:   ValidationService/Models/CreditCard.cs new file:   ValidationService/Properties/AssemblyInfo.cs new file:   ValidationService/ValidationService.csproj new file:   ValidationService/Web.Debug.config new file:   ValidationService/Web.Release.config new file:   ValidationService/Web.config new file:   ValidationService/packages.config

---
+++ ValidationService.Tests/TestAbstractClassValidator.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ValidationService.Tests
+{
+    [TestClass]
+    public class TestAbstractClassValidator : Validator
+    {
+        [TestMethod]
+        void Validate_ReturnCardTypeAmex()
+        {
+            System.DateTime expiry = new System.DateTime();
+            expiry.AddMonths(12);
+            expiry.AddYears(2019);
+
+            var validator = new Validator(345981456357981, expiry);
+            string cardType = validator.Validate();
+
+            Assert.IsNotNull(cardType);
+            Assert.AreEqual("Amex", cardType);
+        }
+
+        [TestMethod]
+        void Validate_ReturnCardTypeJCB()
+        {
+            System.DateTime expiry = new System.DateTime();
+            expiry.AddMonths(12);
+            expiry.AddYears(2019);
+
+            var validator = new Validator(3456789000001234, expiry);
+            string cardType = validator.Validate();
+
+            Assert.IsNotNull(cardType);
+            Assert.AreEqual("JCB", cardType);
+        }
+
+        [TestMethod]
+        void Validate_ReturnCardTypeVisa()
+        {
+            System.DateTime expiry = new System.DateTime();
+            expiry.AddMonths(10);
+            expiry.AddYears(2016);
+
+            var validator = new Validator(4444999910102345, expiry);
+            string cardType = validator.Validate();
+
+            Assert.IsNotNull(cardType);
+            Assert.AreEqual("Visa", cardType);
+        }
+
+        [TestMethod]
+        void Validate_ReturnCardTypeMasterCard()
+        {
+            System.DateTime expiry = new System.DateTime();
+            expiry.AddMonths(2);
+            expiry.AddYears(2017);
+
+            var validator = new Validator(5105999912200110, expiry);
+            string cardType = validator.Validate();
+
+            Assert.IsNotNull(cardType);
+            Assert.AreEqual("Master Card", cardType);
+        }
+
+        [TestMethod]
+        void Validate_ReturnUnknownAndOrInvalidCard()
+        {
+            System.DateTime expiry = new System.DateTime();
+            expiry.AddMonths(9);
+            expiry.AddYears(2019);
+
+            var validator = new Validator(1234567890987654321, expiry);
+            string cardType = validator.Validate();
+
+            Assert.IsNotNull(cardType);
+            Assert.AreEqual("Unknow and/or invalid card", cardType);
+        }
+    }
+}
---
+++ ValidationService/Models/CreditCard.cs
@@ -0,0 +1,10 @@
+﻿namespace ValidationService.Models
+{
+    public class CreditCard
+    {
+        public int Id { get; set; }
+        [System.ComponentModel.DataAnnotations.Range(15, 16, ErrorMessage = "The number must contain either 15 or 16 digits")]
+        public long Number { get; set; }
+        public System.DateTime ExpiryDate { get; set; }
+    }
+}
