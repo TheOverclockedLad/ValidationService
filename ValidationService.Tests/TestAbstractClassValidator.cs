6b277efbbec8a25893335e9181df4b85ada15e1b
TheOverclockedLad <42649194+TheOverclockedLad@users.noreply.github.com>
2019-03-13 14:31:46 +0530

    Added class Validator

--- ValidationService.Tests/TestAbstractClassValidator.cs
+++ ValidationService.Tests/TestAbstractClassValidator.cs
@@ -6,73 +6,58 @@ namespace ValidationService.Tests
     public class TestAbstractClassValidator : Validator
     {
         [TestMethod]
-        void Validate_ReturnCardTypeAmex()
+        public void Validate_ReturnCardTypeAmex()
         {
-            System.DateTime expiry = new System.DateTime();
-            expiry.AddMonths(12);
-            expiry.AddYears(2019);
+            System.DateTime expiry = new System.DateTime(2019, 12, 01);
 
-            var validator = new Validator(345981456357981, expiry);
-            string cardType = validator.Validate();
+            string cardType = Validate(345981456357981, expiry);
 
             Assert.IsNotNull(cardType);
             Assert.AreEqual("Amex", cardType);
         }
 
         [TestMethod]
-        void Validate_ReturnCardTypeJCB()
+        public void Validate_ReturnCardTypeJCB()
         {
-            System.DateTime expiry = new System.DateTime();
-            expiry.AddMonths(12);
-            expiry.AddYears(2019);
+            System.DateTime expiry = new System.DateTime(2019, 12, 01);
 
-            var validator = new Validator(3456789000001234, expiry);
-            string cardType = validator.Validate();
+            string cardType = Validate(3456789000001234, expiry);
 
             Assert.IsNotNull(cardType);
             Assert.AreEqual("JCB", cardType);
         }
 
         [TestMethod]
-        void Validate_ReturnCardTypeVisa()
+        public void Validate_ReturnCardTypeVisa()
         {
-            System.DateTime expiry = new System.DateTime();
-            expiry.AddMonths(10);
-            expiry.AddYears(2016);
+            System.DateTime expiry = new System.DateTime(2016, 10, 01);
 
-            var validator = new Validator(4444999910102345, expiry);
-            string cardType = validator.Validate();
+            string cardType = Validate(4444999910102345, expiry);
 
             Assert.IsNotNull(cardType);
             Assert.AreEqual("Visa", cardType);
         }
 
         [TestMethod]
-        void Validate_ReturnCardTypeMasterCard()
+        public void Validate_ReturnCardTypeMasterCard()
         {
-            System.DateTime expiry = new System.DateTime();
-            expiry.AddMonths(2);
-            expiry.AddYears(2017);
+            System.DateTime expiry = new System.DateTime(2017, 02, 01);
 
-            var validator = new Validator(5105999912200110, expiry);
-            string cardType = validator.Validate();
+            string cardType = Validate(5105999912200110, expiry);
 
             Assert.IsNotNull(cardType);
             Assert.AreEqual("Master Card", cardType);
         }
 
         [TestMethod]
-        void Validate_ReturnUnknownAndOrInvalidCard()
+        public void Validate_ReturnUnknownAndOrInvalidCard()
         {
-            System.DateTime expiry = new System.DateTime();
-            expiry.AddMonths(9);
-            expiry.AddYears(2019);
+            System.DateTime expiry = new System.DateTime(2019, 09, 01);
 
-            var validator = new Validator(1234567890987654321, expiry);
-            string cardType = validator.Validate();
+            string cardType = Validate(1234567890987654321, expiry);
 
             Assert.IsNotNull(cardType);
-            Assert.AreEqual("Unknow and/or invalid card", cardType);
+            Assert.AreEqual("Unknown and/or invalid card", cardType);
         }
     }
 }
